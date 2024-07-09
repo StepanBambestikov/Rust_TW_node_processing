21b556c1d4ee2051f1a27e053f79ba2dd3770482
StepanBambestikov <93329924+StepanBambestikov@users.noreply.github.com>
2024-07-09 15:50:33 +0700

    Update genome_heap.rs

--- src/genome_service/genome_heap.rs
+++ src/genome_service/genome_heap.rs
@@ -1,6 +1,9 @@
 use std::collections::HashMap;
 
 pub(crate) struct GenomeHeap<'a> {
+    //The structure is related to general information 
+    //about the cross-intersection of pseudonodes across all specified genomes
+    
     unique_count: HashMap<String, Vec<usize>>,
     genome_names: &'a Vec<String>
 }
@@ -23,8 +26,10 @@ impl<'a> GenomeHeap<'a>{
 
     pub fn get_data(&mut self) -> String{
         let mut output = String::new();
+        
+        //Cutting off sequences that do not repeat in at least two different genomes
         let mut sorted_values: Vec<_> = self.unique_count.iter().
-            filter(|&(_, values)| {
+            filter(|&(_, values)| { 
                 let mut count = 0;
                 for &elem in values{
                     if elem != 0{
@@ -37,6 +42,7 @@ impl<'a> GenomeHeap<'a>{
                 false
             }).
             collect();
+        //Sorting by total occurrence in all genomes
         sorted_values.sort_by(|a, b| (b.1.iter().sum::<usize>()).cmp(&a.1.iter().sum()));
         for current_genome_name in self.genome_names{
             output.push_str(&*format!("{},", current_genome_name));
@@ -50,6 +56,6 @@ impl<'a> GenomeHeap<'a>{
             }
             output.push('\n');
         }
-        return output;
+        return output; //The text of the file
     }
 }
